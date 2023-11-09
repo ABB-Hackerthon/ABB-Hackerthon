96416e1aa7413c10b07cc57a857d448dd578d3e2
seobstation <ljs8828@naver.com>
2023-11-09 22:00:35 +0900

    :art: Design: 순찰기록 디테일 CSS 뼈대 완료

--- front/src/screens/Record.tsx
+++ front/src/screens/Record.tsx
@@ -17,6 +17,7 @@ const Record = () => {
 	const petrolList = [
 		{
 			patrolDiaryInfo: {
+				patrolNo: 1,
 				userName: "userName",
 				patrolLogAddress: "patrolLogAddress",
 				patrolDate: "patrolDate",
@@ -24,6 +25,7 @@ const Record = () => {
 		},
 		{
 			patrolDiaryInfo: {
+				patrolNo: 2,
 				userName: "userName",
 				patrolLogAddress: "patrolLogAddress",
 				patrolDate: "patrolDate",
@@ -31,6 +33,7 @@ const Record = () => {
 		},
 		{
 			patrolDiaryInfo: {
+				patrolNo: 3,
 				userName: "userName",
 				patrolLogAddress: "patrolLogAddress",
 				patrolDate: "patrolDate",
@@ -38,6 +41,7 @@ const Record = () => {
 		},
 		{
 			patrolDiaryInfo: {
+				patrolNo: 4,
 				userName: "userName",
 				patrolLogAddress: "patrolLogAddress",
 				patrolDate: "patrolDate",
