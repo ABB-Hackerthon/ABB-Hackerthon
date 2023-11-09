9ae900e7b92a766bbd6169a77374e2d0f194c207
sunhee3859 <sunhee3859@naver.com>
2023-11-09 23:08:48 +0900

    front sunhee

--- front/src/constants/constants.ts
+++ front/src/constants/constants.ts
@@ -2,3 +2,4 @@ export const BASE_URL = "https://idog.store/api";
 export const CONTENT_TYPE = "application/json; charset=utf-8";
 export const TIMEOUT = 1000;
 export const ABB_BASE_URL = "http://175.106.96.224:3398";
+//test
